85959be4ba46b5cb4e31fcf4e7c73b2e2190f4bf
blvvvck <theblvvvck@gmail.com>
2018-03-18 11:23:29 +0300

    init project

---
+++ SoulCloudiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoulCloudiOS
+//
+//  Created by BLVCK on 18/03/2018.
+//  Copyright © 2018 BLVCK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
