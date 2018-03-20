aca38dd0e713084b1e4825bae130db79050348e7
DamirZaripov <rubingooddz@gmail.com>
2018-03-20 03:06:09 +0300

    added cell

--- SoulCloudiOS/ViewControllers/TabBarControllers/BookViewController.swift
+++ SoulCloudiOS/ViewControllers/TabBarControllers/BookViewController.swift
@@ -8,28 +8,34 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
-
+class BookViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var isUserHaveBooks = false
+    
+    @IBOutlet weak var tableView: UITableView!
+    let bookCellIdentifier = "bookCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        registerCell()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func registerCell() {
+        let nib = UINib(nibName: "BookTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: bookCellIdentifier)
     }
-    */
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: bookCellIdentifier, for: indexPath) as! BookTableViewCell
+        return cell
+    }
+    
 }
