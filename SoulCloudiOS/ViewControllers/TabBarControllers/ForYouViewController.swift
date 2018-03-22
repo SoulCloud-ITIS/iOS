9a91b53296526dd302f33a0bcec99b45cc250e0e
blvvvck <theblvvvck@gmail.com>
2018-03-22 19:49:23 +0300

    fixed bag when you pull to refresh and crash

--- SoulCloudiOS/ViewControllers/TabBarControllers/ForYouViewController.swift
+++ SoulCloudiOS/ViewControllers/TabBarControllers/ForYouViewController.swift
@@ -129,7 +129,7 @@ class ForYouViewController: UIViewController, UITableViewDelegate, UITableViewDa
     //MARK - Action for pull to refresh
     @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
         
-        getRecomendedBookByGenres()
+        getBooks()
         refreshControl.endRefreshing()
     }
 }
